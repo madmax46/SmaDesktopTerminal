94ee6230372ce5e3d4abf2316d8d9bd50693a674
max <glushakovmax32@gmail.com>
2019-12-12 01:28:04 +0300

    updated charts and updated loading data from service

--- SmaDesktopTerminal/SmaDesktopTerminal/DesktopTerminalWindow.xaml.cs
+++ SmaDesktopTerminal/SmaDesktopTerminal/DesktopTerminalWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using SmaDesktopTerminal.Models;
+﻿using LiveCharts.Wpf;
+using SmaDesktopTerminal.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -31,7 +33,73 @@ namespace SmaDesktopTerminal
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //windowModel.ReloadParsedInstrumentsAsync();
+
+            windowModel.ReloadParsedInstrumentsAsync();
+        }
+
+        private void resizeThumb_DragDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
+        {
+
+            //Move the Thumb to the mouse position during the drag operation
+            //double yadjust = firstColumn. + e.VerticalChange;
+            double xadjust = userControl.Width + e.HorizontalChange;
+            if ((xadjust >= 0))
+            {
+                //firstColumn.Width = new GridLength(xadjust, firstColumn.Width.GridUnitType);
+                userControl.Width = xadjust;
+                //Canvas.SetLeft(myThumb, Canvas.GetLeft(myThumb) +
+                //                        e.HorizontalChange);
+                //Canvas.SetTop(myThumb, Canvas.GetTop(myThumb) +
+                //                        e.VerticalChange);
+                //changes.Text = "Size: " +
+                //                myCanvasStretch.Width.ToString() +
+                //                 ", " +
+                //                myCanvasStretch.Height.ToString();
+            }
+
+        }
+
+        private void parsedInstrView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            windowModel.ReloadCandlesAsync();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            windowModel.ReloadParsedInstrumentsAsync();
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void instrumentsUpdateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            windowModel.ReloadParsedInstrumentsAsync();
+        }
+
+        private void chartUpdateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            windowModel.ReloadCandlesAsync();
+        }
+
+        private void Axis_RangeChanged(LiveCharts.Events.RangeChangedEventArgs eventArgs)
+        {
+            //liveChartOhlc.AxisX[0].SetRange(30, 30);
+            //eventArgs.Range
+            //sync the graphs
+            double max = ((Axis)eventArgs.Axis).MaxValue;
+            double min = max - 30;
+
+            this.liveChartOhlc.AxisX[0].MinValue = min;
+            this.liveChartOhlc.AxisX[0].MaxValue = max;
+
+            this.liveChartVolume.AxisX[0].MinValue = min;
+            this.liveChartVolume.AxisX[0].MaxValue = max;
+
+            //Repeat for as many graphs as you have
         }
     }
 }
--- SmaDesktopTerminal/SmaDesktopTerminal/Models/TerminalWindowModel.cs
+++ SmaDesktopTerminal/SmaDesktopTerminal/Models/TerminalWindowModel.cs
@@ -1,5 +1,13 @@
 ﻿using ExchCommonLib.Classes.Exchange;
+using ExchCommonLib.Classes.Requests;
+using ExchCommonLib.Classes.Responses;
 using ExchCommonLib.Rest;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
 using SmaDesktopTerminal.Classes;
 using SmaDesktopTerminal.Classes.ServiceResponses;
 using System;
@@ -11,7 +19,9 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace SmaDesktopTerminal.Models
 {
@@ -22,8 +32,20 @@ namespace SmaDesktopTerminal.Models
         private readonly string urlToService;
         private readonly string authToken;
         private readonly HttpClient httpClientService;
-        private Person person;
+        private readonly DesktopTerminalWindow desktopTerminalWindow;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private PlotModel candlesChart;
         private ObservableCollection<Instrument> instruments;
+
+        private Person person;
+        private Instrument selectedInstrument;
+        private string selectedChartInterval;
+        private List<string> chartIntervals;
+        private SeriesCollection seriesCollection;
+        private SeriesCollection seriesVolumeCollection;
+        private List<string> seriesLabels;
 
         public Person CurPerson
         {
@@ -37,10 +59,6 @@ namespace SmaDesktopTerminal.Models
                 OnPropertyChanged();
             }
         }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-
         public ObservableCollection<Instrument> Instruments
         {
             get
@@ -50,6 +68,103 @@ namespace SmaDesktopTerminal.Models
             set
             {
                 instruments = value;
+                OnPropertyChanged();
+            }
+
+        }
+        public Instrument SelectedInstrument
+        {
+            get
+            {
+                return selectedInstrument;
+            }
+            set
+            {
+                selectedInstrument = value;
+                OnPropertyChanged();
+            }
+
+        }
+        public List<string> ChartIntervals
+        {
+            get
+            {
+                return chartIntervals;
+            }
+            set
+            {
+                chartIntervals = value;
+                OnPropertyChanged();
+            }
+
+        }
+        public string SelectedChartInterval
+        {
+            get
+            {
+                return selectedChartInterval;
+            }
+            set
+            {
+                selectedChartInterval = value;
+                OnPropertyChanged();
+            }
+
+        }
+
+        public PlotModel CandlesChart
+        {
+            get
+            {
+                return candlesChart;
+            }
+            set
+            {
+                candlesChart = value;
+                OnPropertyChanged();
+            }
+
+        }
+
+
+        public SeriesCollection SeriesCollection
+        {
+            get
+            {
+                return seriesCollection;
+            }
+            set
+            {
+                seriesCollection = value;
+                OnPropertyChanged();
+            }
+
+        }
+        public List<string> Labels
+        {
+            get
+            {
+                return seriesLabels;
+            }
+            set
+            {
+                seriesLabels = value;
+                OnPropertyChanged();
+            }
+
+        }
+
+
+
+        public SeriesCollection SeriesVolumeCollection
+        {
+            get
+            {
+                return seriesVolumeCollection;
+            }
+            set
+            {
+                seriesVolumeCollection = value;
                 OnPropertyChanged();
             }
 
@@ -66,26 +181,48 @@ namespace SmaDesktopTerminal.Models
             httpClientService.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authToken);
             CurPerson = mainModel.CurPerson;
             Instruments = new ObservableCollection<Instrument>();
+            CandlesChart = new PlotModel();
 
+            SeriesCollection = new SeriesCollection();
+            SeriesVolumeCollection = new SeriesCollection();
+            Labels = new List<string>();
 
-            DesktopTerminalWindow desktopTerminalWindow = new DesktopTerminalWindow(this);
+            desktopTerminalWindow = new DesktopTerminalWindow(this);
             desktopTerminalWindow.Show();
 
-            ReloadParsedInstruments();
+            InitIntervals();
+            //ReloadCandles();
         }
 
+        private void InitIntervals()
+        {
+            ChartIntervals = new List<string>()
+            {
+                "1min",
+                "5min",
+                "15min",
+                "30min",
+                "hour",
+                "day",
+                "week",
+                "month"
+            };
+        }
 
-
-        public void ReloadParsedInstruments()
+        public async void ReloadParsedInstrumentsAsync()
         {
             try
             {
-                var res = CallRest.GetAsync<InstrumentsResponse>($"{urlToService}api/v1/exchange/GetParsedInstruments", httpClientService).Result;
-
+                desktopTerminalWindow.instrumentsProgressBar.Visibility = System.Windows.Visibility.Visible;
+                instruments.Clear();
+                var res = await CallRest.GetAsync<InstrumentsResponse>($"{urlToService}api/v1/market/GetParsedInstruments", httpClientService);
+                //Thread.Sleep(10000);
                 if (res?.Response?.Instruments?.Any() == true)
                 {
                     res.Response.Instruments.ForEach(r => Instruments.Add(r));
                 }
+
+                desktopTerminalWindow.instrumentsProgressBar.Visibility = System.Windows.Visibility.Hidden;
             }
             catch (Exception ex)
             {
@@ -95,7 +232,196 @@ namespace SmaDesktopTerminal.Models
 
 
 
+        public void ReloadCandles()
+        {
+            try
+            {
+                VolumeStyle volumeStyle = VolumeStyle.Combined;
+                var series = new CandleStickAndVolumeSeries
+                {
+                    PositiveColor = OxyColors.DarkGreen,
+                    NegativeColor = OxyColors.Red,
+                    PositiveHollow = false,
+                    NegativeHollow = false,
+                    SeparatorColor = OxyColors.Gray,
+                    SeparatorLineStyle = LineStyle.Dash,
+                    VolumeStyle = volumeStyle,
+                    //StrokeThickness = 3
+                };
 
+
+                series.Append(new OhlcvItem(1, 3, 5, 1, 4));
+                series.Append(new OhlcvItem(2, 4, 5, 1, 3));
+                Random random = new Random();
+                for (int i = 3; i < 20; i++)
+                {
+                    var item = random.Next(1, i);
+                    var item2 = random.Next(1, i);
+                    var item3 = random.Next(1, i);
+                    var item4 = random.Next(1, i);
+
+                    series.Append(new OhlcvItem(i, item, item3, item4, item2));
+                }
+
+                //desktopTerminalWindow.oxyPlotView.InvalidatePlot(true);
+                PlotModel plotModel = new PlotModel();
+                //CandlesChart.Series.Add(series);
+                plotModel.Series.Add(series);
+                CandlesChart = plotModel;
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+
+        public async void ReloadCandlesAsync()
+        {
+            try
+            {
+
+                try
+                {
+                    if (SelectedInstrument == null)
+                    {
+                        return;
+                    }
+
+
+                    desktopTerminalWindow.chartProgressBar.Visibility = System.Windows.Visibility.Visible;
+                    CandlesRequest candlesRequest = new CandlesRequest()
+                    {
+                        InstrumentId = (uint)SelectedInstrument.FinamEmitentIDInt,
+                        Interval = selectedChartInterval,
+                        DateStart = new DateTime(2019, 1, 1),
+                        DateEnd = new DateTime(2020, 1, 1)
+                    };
+                    var res = await CallRest.PostAsync<CandlesRequest, CandlesResponse>($"{urlToService}api/v1/market/candles", candlesRequest, httpClientService);
+
+                    if (res?.Response?.Candles?.Any() == true)
+                    {
+                        FillPlotByNewData(res.Response);
+                    }
+
+                    desktopTerminalWindow.chartProgressBar.Visibility = System.Windows.Visibility.Hidden;
+                }
+                catch (Exception ex)
+                {
+
+                }
+
+
+                ///api/v{version}/market/candles
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        //private void FillPlotByNewData(CandlesResponse candlesResponse)
+        //{
+        //    VolumeStyle volumeStyle = VolumeStyle.Stacked;
+        //    var series = new CandleStickAndVolumeSeries
+        //    {
+        //        PositiveColor = OxyColors.Green,
+        //        NegativeColor = OxyColors.Red,
+        //        PositiveHollow = false,
+        //        NegativeHollow = false,
+        //        SeparatorColor = OxyColors.Gray,
+        //        SeparatorLineStyle = LineStyle.Dash,
+        //        VolumeStyle = volumeStyle,
+        //        //StrokeThickness = 3
+        //    };
+        //    //CandlesChart.Series.Clear();
+        //    //CandlesChart.Series.Add(series);
+
+        //    double i = 1;
+        //    foreach (var oneCandle in candlesResponse.Candles.Take(50))
+        //    {
+        //        var time = DateTimeAxis.ToDouble(oneCandle.Date);
+        //        series.Append(new OhlcvItem(time, oneCandle.Open, oneCandle.High, oneCandle.Low, oneCandle.Close));
+        //        i++;
+        //    }
+        //    OnPropertyChanged("CandlesChart");
+        //    //series.Append(new OhlcvItem(1, 3, 5, 1, 4));
+        //    //series.Append(new OhlcvItem(2, 4, 5, 1, 3));
+        //    //Random random = new Random();
+        //    //for (int i = 3; i < 20; i++)
+        //    //{
+        //    //    var item = random.Next(1, i);
+        //    //    series.Append(new OhlcvItem(i, item, item + 10, item - 3, item + 1));
+        //    //}
+
+        //    PlotModel plotModel = new PlotModel();
+        //    //CandlesChart.Series.Add(series);
+        //    plotModel.Series.Add(series);
+        //    CandlesChart = plotModel;
+
+
+        //}
+        private void FillPlotByNewData(CandlesResponse candlesResponse)
+        {
+            //SeriesCollection = new SeriesCollection();
+            //SeriesVolumeCollection = new SeriesCollection();
+            //Labels = new List<string>();
+
+            SeriesCollection = new SeriesCollection();
+            SeriesVolumeCollection = new SeriesCollection();
+            Labels = new List<string>();
+
+            var series = new CandleSeries()
+            {
+                Values = new ChartValues<OhlcPoint>(),
+            };
+
+            var volumeSeries = new LiveCharts.Wpf.ColumnSeries()
+            {
+                Title = "Объем",
+                Values = new ChartValues<double>()
+            };
+
+            var labels = new List<string>();
+
+            //  VolumeStyle volumeStyle = VolumeStyle.Stacked;
+            //var series = new CandleStickAndVolumeSeries
+            //{
+            //    PositiveColor = OxyColors.Green,
+            //    NegativeColor = OxyColors.Red,
+            //    PositiveHollow = false,
+            //    NegativeHollow = false,
+            //    SeparatorColor = OxyColors.Gray,
+            //    SeparatorLineStyle = LineStyle.Dash,
+            //    VolumeStyle = volumeStyle,
+            //    //StrokeThickness = 3
+            //};
+            //CandlesChart.Series.Clear();
+            //CandlesChart.Series.Add(series);
+            //series.
+            //GearedValues
+            //desktopTerminalWindow.liveChartOhlc. = ScrollMode.X;
+
+
+            //GearedValues
+
+            foreach (var oneCandle in candlesResponse.Candles.Take(100))
+            {
+                var time = DateTimeAxis.ToDouble(oneCandle.Date);
+                series.Values.Add(new OhlcPoint(oneCandle.Open, oneCandle.High, oneCandle.Low, oneCandle.Close));
+                volumeSeries.Values.Add((double)oneCandle.Volume);
+                labels.Add(oneCandle.Date.ToString());
+            }
+
+            SeriesCollection.Add(series);
+            SeriesVolumeCollection.Add(volumeSeries);
+            Labels = labels;
+            //desktopTerminalWindow.liveChartOhlc.AxisX[0].MinValue = desktopTerminalWindow.liveChartOhlc.AxisX[0].MaxValue - 1;
+
+            //desktopTerminalWindow.liveChartOhlc.AxisX[0].SetRange(30, 30);
+        }
 
         private void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
